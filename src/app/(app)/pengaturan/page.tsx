7e74e7598768a6ffae1c403d18adbe3d2bd982b4
Ibnu Wibowo <ibnuw83@gmail.com>
2025-11-14 17:27:08 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/app/(app)/pengaturan/page.tsx
+++ src/app/(app)/pengaturan/page.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { useToast } from '@/hooks/use-toast';
 import { allData, updateAllData } from '@/lib/data';
 import type { AppSettings } from '@/lib/types';
